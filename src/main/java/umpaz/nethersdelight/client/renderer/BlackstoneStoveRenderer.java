33a6ee6df0785c74869b383ba05e2e27c9aa4085
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-29 17:38:44 +0100

    update BlackstoneStoveRenderer to use joml vectors

--- src/main/java/umpaz/nethersdelight/client/renderer/BlackstoneStoveRenderer.java
+++ src/main/java/umpaz/nethersdelight/client/renderer/BlackstoneStoveRenderer.java
@@ -1,24 +1,30 @@
 package umpaz.nethersdelight.client.renderer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.Vec2;
 import net.minecraftforge.items.ItemStackHandler;
+import org.joml.AxisAngle4d;
+import org.joml.Quaternionf;
 import umpaz.nethersdelight.common.block.BlackstoneStoveBlock;
 import umpaz.nethersdelight.common.block.entity.BlackstoneStoveBlockEntity;
+import org.joml.AxisAngle4f;
+import java.lang.Math;
 
 public class BlackstoneStoveRenderer implements BlockEntityRenderer<BlackstoneStoveBlockEntity>
 {
     public BlackstoneStoveRenderer(BlockEntityRendererProvider.Context context) {
     }
+
+    private static final Quaternionf itemPoseXRotation = new Quaternionf(new AxisAngle4f((float)Math.toRadians(90), 1F, 0F, 0F));
 
     @Override
     public void render(BlackstoneStoveBlockEntity blackstoneStoveEntity, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int combinedLightIn, int combinedOverlayIn) {
@@ -36,11 +42,11 @@ public class BlackstoneStoveRenderer implements BlockEntityRenderer<BlackstoneSt
                 poseStack.translate(0.5D, 1.02D, 0.5D);
 
                 // Rotate item to face the stove's front side
-                float f = -direction.toYRot();
-                poseStack.mulPose(Vector3f.YP.rotationDegrees(f));
+                float f = (float)Math.toRadians(-direction.toYRot());
+                poseStack.mulPose(new Quaternionf(new AxisAngle4f(f, 0F, 1F, 0F)));
 
                 // Rotate item flat on the stove. Use X and Y from now on
-                poseStack.mulPose(Vector3f.XP.rotationDegrees(90.0F));
+                poseStack.mulPose(itemPoseXRotation);
 
                 // Neatly align items according to their index
                 Vec2 itemOffset = blackstoneStoveEntity.getStoveItemOffset(i);
