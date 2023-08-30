93c196a291f54b9616c086610a1a00662de4abd3
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-08-30 04:20:53 +0100

    use more concise syntax when registering data providers

--- src/main/java/umpaz/nethersdelight/data/NDDataGenerators.java
+++ src/main/java/umpaz/nethersdelight/data/NDDataGenerators.java
@@ -2,14 +2,12 @@ package umpaz.nethersdelight.data;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import umpaz.nethersdelight.NethersDelight;
-import umpaz.nethersdelight.data.NDRecipes;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -17,23 +15,17 @@ import java.util.concurrent.CompletableFuture;
 public class NDDataGenerators
 {
     @SubscribeEvent
-    public static void gatherData(GatherDataEvent event) {
+    public static void onGatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
+        PackOutput output = generator.getPackOutput();
         CompletableFuture<HolderLookup.Provider> registries = event.getLookupProvider();
         ExistingFileHelper fileHelper = event.getExistingFileHelper();
 
-        DataProvider.Factory<NDBlockTags> blockTagsFactory = (PackOutput output) -> {
-            return new NDBlockTags(output, registries, fileHelper);
-        };
-        NDBlockTags blockTagsProvider = generator.addProvider(event.includeServer(), blockTagsFactory);
-
-        DataProvider.Factory<NDItemTags> itemTagsFactory = (PackOutput output) -> {
-            return new NDItemTags(output, registries, blockTagsProvider.contentsGetter(), fileHelper);
-        };
-        NDItemTags itemTagsProvider = generator.addProvider(event.includeServer(), itemTagsFactory);
-
-        generator.addProvider(event.includeServer(), (DataProvider.Factory<NDLang>)NDLang::new);
-        generator.addProvider(event.includeServer(), (DataProvider.Factory<NDRecipes>)NDRecipes::new);
+        NDBlockTags blockTagsProvider = generator.addProvider(event.includeServer(), new NDBlockTags(output, registries, fileHelper));
+        NDItemTags itemTagsProvider = generator.addProvider(event.includeServer(), new NDItemTags(output, registries, blockTagsProvider.contentsGetter(), fileHelper));
+        generator.addProvider(event.includeServer(), new NDRecipes(output));
         //generator.addProvider(event.includeServer(), new Advancements(generator));
+
+        generator.addProvider(event.includeClient(), new NDLang(output));
     }
 }
