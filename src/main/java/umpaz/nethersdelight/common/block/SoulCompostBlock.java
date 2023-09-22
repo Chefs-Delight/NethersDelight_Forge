2e6c5068781b298a9eaf4e8c63e6cbfcc12bf993
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-09-22 05:32:48 +0100

    implement canSustainPlant on SoulCompostBlock

--- src/main/java/umpaz/nethersdelight/common/block/SoulCompostBlock.java
+++ src/main/java/umpaz/nethersdelight/common/block/SoulCompostBlock.java
@@ -1,13 +1,16 @@
 package umpaz.nethersdelight.common.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FungusBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
@@ -93,5 +96,14 @@ public class SoulCompostBlock extends Block
                 level.addParticle(ParticleTypes.MYCELIUM, (double) pos.getX() + (double) random.nextFloat(), (double) pos.getY() + 1.1D, (double) pos.getZ() + (double) random.nextFloat(), 0.0D, 0.0D, 0.0D);
             }
         }
+    }
+
+    @Override
+    public boolean canSustainPlant(BlockState state, BlockGetter level, BlockPos pos, Direction facing, net.minecraftforge.common.IPlantable plantable) {
+        BlockState plant = plantable.getPlant(level, pos.relative(facing));
+        Block plantBlock = plant.getBlock();
+        if (plantBlock instanceof FungusBlock) return true;
+
+        return false;
     }
 }
